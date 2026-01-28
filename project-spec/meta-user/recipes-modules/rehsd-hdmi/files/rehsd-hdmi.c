3d77f14ef88087efa11b9c13bb3e2acc1967a629
2827700630 <2827700630@users.noreply.github.com>
2026-01-28 12:44:56 +0000

    取消了设置时钟

--- project-spec/meta-user/recipes-modules/rehsd-hdmi/files/rehsd-hdmi.c
+++ project-spec/meta-user/recipes-modules/rehsd-hdmi/files/rehsd-hdmi.c
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
-* Adapted from Digilent, Author : Cosmin Tanislav <demonsingur@gmail.com>
-* Modified: Add debug logs for error locating
-*/
+ * Adapted from Digilent, Author : Cosmin Tanislav <demonsingur@gmail.com>
+ * Modified: Add debug logs for error locating
+ */
 
 #include <drm/drm_atomic_helper.h>
 #include <drm/drm_crtc.h>
@@ -18,7 +18,8 @@
 
 #include <linux/platform_device.h>
 
-struct rehsd_hdmi {
+struct rehsd_hdmi
+{
 	struct drm_encoder encoder;
 	struct drm_connector connector;
 	struct drm_device *drm_dev;
@@ -47,9 +48,11 @@ static int rehsd_hdmi_get_modes(struct drm_connector *connector)
 
 	dev_dbg(hdmi->dev, "[%s] Enter get_modes, i2c_bus=%p\n", __func__, hdmi->i2c_bus);
 
-	if (hdmi->i2c_bus) {
+	if (hdmi->i2c_bus)
+	{
 		edid = drm_get_edid(connector, hdmi->i2c_bus);
-		if (!edid) {
+		if (!edid)
+		{
 			dev_err(hdmi->dev, "[%s] Failed to get EDID data from i2c bus\n", __func__);
 			return 0;
 		}
@@ -58,7 +61,9 @@ static int rehsd_hdmi_get_modes(struct drm_connector *connector)
 		count = drm_add_edid_modes(connector, edid);
 		dev_dbg(hdmi->dev, "[%s] Got EDID, added %d modes\n", __func__, count);
 		kfree(edid);
-	} else {
+	}
+	else
+	{
 		count = drm_add_modes_noedid(connector, hdmi->hmax, hdmi->vmax);
 		drm_set_preferred_mode(connector, hdmi->hpref, hdmi->vpref);
 		dev_dbg(hdmi->dev, "[%s] No i2c bus, add no-edid modes (hmax=%d, vmax=%d), pref(%d,%d), count=%d\n",
@@ -67,7 +72,7 @@ static int rehsd_hdmi_get_modes(struct drm_connector *connector)
 
 	// 原代码返回0是错误的？这里保留原逻辑但添加调试
 	dev_dbg(hdmi->dev, "[%s] Exit, return count=%d (NOTE: original code returns 0)\n", __func__, count);
-	return 0;
+	return count;
 }
 
 static int rehsd_hdmi_mode_valid(struct drm_connector *connector, struct drm_display_mode *mode)
@@ -76,7 +81,8 @@ static int rehsd_hdmi_mode_valid(struct drm_connector *connector, struct drm_dis
 
 	dev_dbg(hdmi->dev, "[%s] Enter, mode=%p\n", __func__, mode);
 
-	if (!mode) {
+	if (!mode)
+	{
 		dev_dbg(hdmi->dev, "[%s] Mode is NULL, return MODE_BAD\n", __func__);
 		goto mode_bad;
 	}
@@ -84,12 +90,14 @@ static int rehsd_hdmi_mode_valid(struct drm_connector *connector, struct drm_dis
 	dev_dbg(hdmi->dev, "[%s] Mode info: clock=%d, hdisplay=%d, vdisplay=%d, flags=0x%x\n",
 			__func__, mode->clock, mode->hdisplay, mode->vdisplay, mode->flags);
 
-	if (mode->flags & (DRM_MODE_FLAG_INTERLACE | DRM_MODE_FLAG_DBLCLK | DRM_MODE_FLAG_3D_MASK)) {
+	if (mode->flags & (DRM_MODE_FLAG_INTERLACE | DRM_MODE_FLAG_DBLCLK | DRM_MODE_FLAG_3D_MASK))
+	{
 		dev_dbg(hdmi->dev, "[%s] Mode has invalid flags (interlace/dblclk/3d), return MODE_BAD\n", __func__);
 		goto mode_bad;
 	}
 
-	if (mode->clock > hdmi->fmax || mode->hdisplay > hdmi->hmax || mode->vdisplay > hdmi->vmax) {
+	if (mode->clock > hdmi->fmax || mode->hdisplay > hdmi->hmax || mode->vdisplay > hdmi->vmax)
+	{
 		dev_dbg(hdmi->dev, "[%s] Mode out of limit: clock(%d>%d) OR hdisplay(%d>%d) OR vdisplay(%d>%d), return MODE_BAD\n",
 				__func__, mode->clock, hdmi->fmax, mode->hdisplay, hdmi->hmax, mode->vdisplay, hdmi->vmax);
 		goto mode_bad;
@@ -122,7 +130,8 @@ static enum drm_connector_status rehsd_hdmi_detect(struct drm_connector *connect
 
 	dev_dbg(hdmi->dev, "[%s] Enter, force=%d, i2c_bus=%p\n", __func__, force, hdmi->i2c_bus);
 
-	if (!hdmi->i2c_bus) {
+	if (!hdmi->i2c_bus)
+	{
 		dev_dbg(hdmi->dev, "[%s] No i2c bus, return connector_status_unknown\n", __func__);
 		return connector_status_unknown;
 	}
@@ -165,8 +174,9 @@ static int rehsd_hdmi_create_connector(struct rehsd_hdmi *hdmi)
 	connector->polled = DRM_CONNECTOR_POLL_CONNECT | DRM_CONNECTOR_POLL_DISCONNECT;
 
 	ret = drm_connector_init(hdmi->drm_dev, connector,
-			&rehsd_hdmi_connector_funcs, DRM_MODE_CONNECTOR_HDMIA);
-	if (ret) {
+							 &rehsd_hdmi_connector_funcs, DRM_MODE_CONNECTOR_HDMIA);
+	if (ret)
+	{
 		dev_err(hdmi->dev, "[%s] Failed to initialize connector, ret=%d\n", __func__, ret);
 		return ret;
 	}
@@ -180,26 +190,33 @@ static int rehsd_hdmi_create_connector(struct rehsd_hdmi *hdmi)
 }
 
 static void rehsd_hdmi_atomic_mode_set(struct drm_encoder *encoder,
-		struct drm_crtc_state *crtc_state, struct drm_connector_state *connector_state)
+									   struct drm_crtc_state *crtc_state, struct drm_connector_state *connector_state)
 {
 	struct rehsd_hdmi *hdmi = encoder_to_hdmi(encoder);
 	struct drm_display_mode *m = &crtc_state->adjusted_mode;
 	unsigned long target_rate = m->clock * 1000;
+	printk(KERN_INFO "HDMI: m->clock = %lu, target_rate = %lu (expected: 74250000)\n", 
+       m->clock, target_rate);
+
 	int ret;
 
 	dev_dbg(hdmi->dev, "[%s] Enter, encoder=%p, target clock=%d KHz (raw=%lu Hz)\n",
 			__func__, encoder, m->clock, target_rate);
 
-	if (IS_ERR_OR_NULL(hdmi->clk)) {
+	if (IS_ERR_OR_NULL(hdmi->clk))
+	{
 		dev_err(hdmi->dev, "[%s] HDMI clk is invalid (clk=%p)\n", __func__, hdmi->clk);
 		return;
 	}
 
-	ret = clk_set_rate(hdmi->clk, target_rate);
-	if (ret) {
+	//ret = clk_set_rate(hdmi->clk, target_rate);
+	if (ret)
+	{
 		dev_err(hdmi->dev, "[%s] Failed to set clk rate to %lu Hz, ret=%d\n",
 				__func__, target_rate, ret);
-	} else {
+	}
+	else
+	{
 		dev_dbg(hdmi->dev, "[%s] Clk rate set to %lu Hz (actual: %lu Hz)\n",
 				__func__, target_rate, clk_get_rate(hdmi->clk));
 	}
@@ -211,8 +228,8 @@ static void rehsd_hdmi_enable(struct drm_encoder *encoder)
 	dev_dbg(hdmi->dev, "[%s] Enter, encoder=%p, clk_enabled=%d\n",
 			__func__, encoder, hdmi->clk_enabled);
 
-	/*
-	if (hdmi->clk_enabled) {
+	if (hdmi->clk_enabled)
+	{
 		dev_dbg(hdmi->dev, "[%s] Clk already enabled, skip\n", __func__);
 		return;
 	}
@@ -220,7 +237,7 @@ static void rehsd_hdmi_enable(struct drm_encoder *encoder)
 	clk_prepare_enable(hdmi->clk);
 	hdmi->clk_enabled = true;
 	dev_dbg(hdmi->dev, "[%s] Clk enabled successfully\n", __func__);
-	*/
+
 	return;
 }
 
@@ -230,8 +247,8 @@ static void rehsd_hdmi_disable(struct drm_encoder *encoder)
 	dev_dbg(hdmi->dev, "[%s] Enter, encoder=%p, clk_enabled=%d\n",
 			__func__, encoder, hdmi->clk_enabled);
 
-	/*
-	if (!hdmi->clk_enabled) {
+	if (!hdmi->clk_enabled)
+	{
 		dev_dbg(hdmi->dev, "[%s] Clk already disabled, skip\n", __func__);
 		return;
 	}
@@ -239,7 +256,7 @@ static void rehsd_hdmi_disable(struct drm_encoder *encoder)
 	clk_disable_unprepare(hdmi->clk);
 	hdmi->clk_enabled = false;
 	dev_dbg(hdmi->dev, "[%s] Clk disabled successfully\n", __func__);
-	*/
+
 	return;
 }
 
@@ -262,8 +279,9 @@ static int rehsd_hdmi_create_encoder(struct rehsd_hdmi *hdmi)
 
 	encoder->possible_crtcs = 1;
 	ret = drm_encoder_init(hdmi->drm_dev, encoder,
-			&rehsd_hdmi_encoder_funcs, DRM_MODE_ENCODER_TMDS, NULL);
-	if (ret) {
+						   &rehsd_hdmi_encoder_funcs, DRM_MODE_ENCODER_TMDS, NULL);
+	if (ret)
+	{
 		dev_err(hdmi->dev, "[%s] Failed to initialize encoder, ret=%d\n", __func__, ret);
 		return ret;
 	}
@@ -284,13 +302,15 @@ static int rehsd_hdmi_bind(struct device *dev, struct device *master, void *data
 	hdmi->drm_dev = data;
 
 	ret = rehsd_hdmi_create_encoder(hdmi);
-	if (ret) {
+	if (ret)
+	{
 		dev_err(dev, "[%s] Failed to create encoder, ret=%d\n", __func__, ret);
 		goto encoder_create_fail;
 	}
 
 	ret = rehsd_hdmi_create_connector(hdmi);
-	if (ret) {
+	if (ret)
+	{
 		dev_err(dev, "[%s] Failed to create connector, ret=%d\n", __func__, ret);
 		goto hdmi_create_fail;
 	}
@@ -331,7 +351,7 @@ static int rehsd_hdmi_parse_dt(struct rehsd_hdmi *hdmi)
 	struct device *dev = hdmi->dev;
 	struct device_node *node = dev->of_node;
 	struct device_node *i2c_node;
-	// int ret;
+	int ret;
 
 	dev_dbg(dev, "[%s] Enter, node=%p\n", __func__, node);
 
@@ -345,67 +365,87 @@ static int rehsd_hdmi_parse_dt(struct rehsd_hdmi *hdmi)
 	dev_dbg(dev, "[%s] Default params: fmax=%d, hmax=%d, vmax=%d, hpref=%d, vpref=%d\n",
 			__func__, hdmi->fmax, hdmi->hmax, hdmi->vmax, hdmi->hpref, hdmi->vpref);
 
-	/*
 	hdmi->clk = devm_clk_get(dev, "clk");
-	if (IS_ERR(hdmi->clk)) {
+	if (IS_ERR(hdmi->clk))
+	{
 		ret = PTR_ERR(hdmi->clk);
 		dev_err(dev, "[%s] Failed to get hdmi clock, ret=%d\n", __func__, ret);
 		return ret;
 	}
 	dev_dbg(dev, "[%s] Got clk=%p\n", __func__, hdmi->clk);
 
+	/*
 	i2c_node = of_parse_phandle(node, "rehsd,edid-i2c", 0);
-	if (i2c_node) {
+	if (i2c_node)
+	{
 		hdmi->i2c_bus = of_get_i2c_adapter_by_node(i2c_node);
 		of_node_put(i2c_node);
 		dev_dbg(dev, "[%s] Parsed edid-i2c phandle, i2c_bus=%p\n", __func__, hdmi->i2c_bus);
 
-		if (!hdmi->i2c_bus) {
+		if (!hdmi->i2c_bus)
+		{
 			ret = -EPROBE_DEFER;
 			dev_err(dev, "[%s] Failed to get edid i2c adapter, ret=%d\n", __func__, ret);
 			return ret;
 		}
-	} else {
+	}
+	else
+	{
 		dev_info(dev, "[%s] Failed to find edid i2c property\n", __func__);
 	}
 
 	ret = of_property_read_u32(node, "rehsd,fmax", &hdmi->fmax);
-	if (ret < 0) {
+	if (ret < 0)
+	{
 		dev_dbg(dev, "[%s] No rehsd,fmax property, use default=%d\n", __func__, rehsd_ENC_MAX_FREQ);
 		hdmi->fmax = rehsd_ENC_MAX_FREQ;
-	} else {
+	}
+	else
+	{
 		dev_dbg(dev, "[%s] Read rehsd,fmax=%d\n", __func__, hdmi->fmax);
 	}
 
 	ret = of_property_read_u32(node, "rehsd,hmax", &hdmi->hmax);
-	if (ret < 0) {
+	if (ret < 0)
+	{
 		dev_dbg(dev, "[%s] No rehsd,hmax property, use default=%d\n", __func__, rehsd_ENC_MAX_H);
 		hdmi->hmax = rehsd_ENC_MAX_H;
-	} else {
+	}
+	else
+	{
 		dev_dbg(dev, "[%s] Read rehsd,hmax=%d\n", __func__, hdmi->hmax);
 	}
 
 	ret = of_property_read_u32(node, "rehsd,vmax", &hdmi->vmax);
-	if (ret < 0) {
+	if (ret < 0)
+	{
 		dev_dbg(dev, "[%s] No rehsd,vmax property, use default=%d\n", __func__, rehsd_ENC_MAX_V);
 		hdmi->vmax = rehsd_ENC_MAX_V;
-	} else {
+	}
+	else
+	{
 		dev_dbg(dev, "[%s] Read rehsd,vmax=%d\n", __func__, hdmi->vmax);
 	}
 
 	ret = of_property_read_u32(node, "rehsd,hpref", &hdmi->hpref);
-	if (ret < 0) {
+	if (ret < 0)
+	{
 		dev_dbg(dev, "[%s] No rehsd,hpref property, use default=%d\n", __func__, rehsd_ENC_PREF_H);
 		hdmi->hpref = rehsd_ENC_PREF_H;
-	} else {
+	}
+	else
+	{
 		dev_dbg(dev, "[%s] Read rehsd,hpref=%d\n", __func__, hdmi->hpref);
 	}
 
 	ret = of_property_read_u32(node, "rehsd,vpref", &hdmi->vpref);
-	if (ret < 0) {
+	if (ret < 0)
+	{
 		dev_dbg(dev, "[%s] No rehsd,vpref property, use default=%d\n", __func__, rehsd_ENC_PREF_V);
 		hdmi->vpref = rehsd_ENC_PREF_V;
-	} else {
+	}
+	else
+	{
 		dev_dbg(dev, "[%s] Read rehsd,vpref=%d\n", __func__, hdmi->vpref);
 	}
 	*/
@@ -426,7 +466,8 @@ static int rehsd_hdmi_probe(struct platform_device *pdev)
 	dev_dbg(dev, "[%s] Enter, pdev=%p\n", __func__, pdev);
 
 	hdmi = devm_kzalloc(dev, sizeof(*hdmi), GFP_KERNEL);
-	if (!hdmi) {
+	if (!hdmi)
+	{
 		ret = -ENOMEM;
 		dev_err(dev, "[%s] Failed to allocate hdmi struct, ret=%d\n", __func__, ret);
 		return ret;
@@ -436,7 +477,8 @@ static int rehsd_hdmi_probe(struct platform_device *pdev)
 	hdmi->dev = dev;
 
 	ret = rehsd_hdmi_parse_dt(hdmi);
-	if (ret) {
+	if (ret)
+	{
 		dev_err(dev, "[%s] Failed to parse device tree, ret=%d\n", __func__, ret);
 		return ret;
 	}
@@ -446,7 +488,8 @@ static int rehsd_hdmi_probe(struct platform_device *pdev)
 	dev_dbg(dev, "[%s] Set drvdata to %p\n", __func__, hdmi);
 
 	ret = component_add(dev, &rehsd_hdmi_component_ops);
-	if (ret < 0) {
+	if (ret < 0)
+	{
 		dev_err(dev, "[%s] Failed to add component, ret=%d\n", __func__, ret);
 		return ret;
 	}
@@ -466,7 +509,8 @@ static void rehsd_hdmi_remove(struct platform_device *pdev)
 	component_del(&pdev->dev, &rehsd_hdmi_component_ops);
 	dev_dbg(dev, "[%s] Removed component\n", __func__);
 
-	if (hdmi->i2c_bus) {
+	if (hdmi->i2c_bus)
+	{
 		i2c_put_adapter(hdmi->i2c_bus);
 		dev_dbg(dev, "[%s] Put i2c adapter %p\n", __func__, hdmi->i2c_bus);
 	}
@@ -475,9 +519,8 @@ static void rehsd_hdmi_remove(struct platform_device *pdev)
 }
 
 static const struct of_device_id rehsd_hdmi_of_match[] = {
-	{ .compatible = "rehsd,hdmi"},
-	{ }
-};
+	{.compatible = "rehsd,hdmi"},
+	{}};
 MODULE_DEVICE_TABLE(of, rehsd_hdmi_of_match);
 
 static struct platform_driver hdmi_driver = {
